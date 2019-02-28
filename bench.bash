264b5cf6d0899329b0775c88c8f234c8ca17d66c
pandaman64 <kointosudesuyo@infoseek.jp>
2019-02-28 01:11:01 +0000

    skip bench if done

--- bench.bash
+++ bench.bash
@@ -14,7 +14,7 @@ echo "Machine: "$MACHINE
 # the next cron job - the timeout value is in seconds and can be adjusted.
 
 # On Linux - using taskset -c 0 ensures that the same core is always used when running the benchmarks.
-taskset -c 0 asv run HEAD~1..HEAD || true
+taskset -c 0 asv run HEAD~1..HEAD --skip-existing || true
 # timeout 7200 taskset -c 0 asv run ALL --skip-existing-commits || true
 
 # On MacOSX:
